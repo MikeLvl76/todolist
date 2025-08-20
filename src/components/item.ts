44f4f616aa3ea37c091f5fa080f65380cafae955
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-08-20 02:26:29 +0200

    add icon with lucide-angular lib and change icon color according to priority

--- src/components/item.ts
+++ src/components/item.ts
@@ -1,13 +1,35 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { LucideAngularModule, TriangleAlertIcon } from 'lucide-angular';
+import { TodoItem } from '../utils/types';
 
 @Component({
+  imports: [LucideAngularModule],
   selector: 'item-container',
   templateUrl: `../templates/item.template.html`,
-  styleUrl: '../styles.css'
+  styleUrl: '../styles.css',
 })
 export class Item {
+  readonly TriangleAlertIcon = TriangleAlertIcon;
   description = input('');
-  priority = input('normal');
+  priority = input<TodoItem['priority']>('normal');
   title = input();
-  deadline = input<Date>();
+  deadline = input<TodoItem['deadline']>();
+
+  formatDate(date?: Date) {
+    if (!date) return 'Unknown date';
+    return date.toLocaleString('fr-FR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  }
+
+  getPriorityColor(priority?: TodoItem['priority']) {
+    if (priority) {
+      if (priority == 'low') return '#70a250';
+      if (priority == 'normal') return '#f2dd50';
+      if (priority == 'high') return '#ed2015';
+    }
+    return '#000000';
+  }
 }
